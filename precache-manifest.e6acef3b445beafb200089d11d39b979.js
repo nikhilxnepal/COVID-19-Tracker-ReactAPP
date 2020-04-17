d2b87c9a3a5d4566ba69c9fe08c089407d56d09d
Nikhil Nepal <nikhilnepal@users.noreply.github.com>
2020-04-17 08:38:13 +0545

    Updates

---
+++ precache-manifest.e6acef3b445beafb200089d11d39b979.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1098851fffd3a17218c073f10e197f84",
+    "url": "/COVID-19-Tracker-ReactAPP/index.html"
+  },
+  {
+    "revision": "fe4c631be2b9a09c34e7",
+    "url": "/COVID-19-Tracker-ReactAPP/static/css/main.4c751b27.chunk.css"
+  },
+  {
+    "revision": "f9fd2d41b9fd880d360a",
+    "url": "/COVID-19-Tracker-ReactAPP/static/js/2.50dd17b7.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19-Tracker-ReactAPP/static/js/2.50dd17b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe4c631be2b9a09c34e7",
+    "url": "/COVID-19-Tracker-ReactAPP/static/js/main.37a7034d.chunk.js"
+  },
+  {
+    "revision": "23cdb3f8156b3e45614a",
+    "url": "/COVID-19-Tracker-ReactAPP/static/js/runtime-main.9994802f.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19-Tracker-ReactAPP/static/media/image.d7265326.png"
+  }
+]);
